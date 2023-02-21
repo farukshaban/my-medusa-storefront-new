65f885e3de2e75e1816c7adfda716d68a099515a
Faruk Shaban <88596384+farukshaban@users.noreply.github.com>
2023-02-21 11:14:18 +0100

    change Acme text to Busy

--- src/modules/account/components/register/index.tsx
+++ src/modules/account/components/register/index.tsx
@@ -43,9 +43,9 @@ const Register = () => {
 
   return (
     <div className="max-w-sm flex flex-col items-center mt-12">
-      <h1 className="text-large-semi uppercase mb-6">Become a Acme Member</h1>
+      <h1 className="text-large-semi uppercase mb-6">Become a Busy Member</h1>
       <p className="text-center text-base-regular text-gray-700 mb-4">
-        Create your Acme Member profile, and get access to an enhanced shopping
+        Create your Busy Member profile, and get access to an enhanced shopping
         experience.
       </p>
       <form className="w-full flex flex-col" onSubmit={onSubmit}>
